85a31565039ff45ac3b75851d0866c32c3f79530
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2024-07-01 11:25:39 +0530

    commit

--- context/src/components/Profile.jsx
+++ context/src/components/Profile.jsx
@@ -6,7 +6,7 @@ function Profile(){
     
     if(!user) return <div>Please Login</div>
 
-    return <div>Welcome {user.username}</div>
+    return <div>Welcome {user.username} <br />your password is {user.password}</div>
 }
 
 export default Profile;
